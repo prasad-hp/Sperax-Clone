ebab41e9a951a5a9cfd27abd3eecaa201ba70548
Prasad Hp <prasadhp@outlook.in>
2024-06-28 00:29:43 +0530

    Initialized Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+
+  return (
+    <>
+      <p className='font-bold text-7xl'>Hello</p>
+    </>
+  )
+}
+
+export default App
